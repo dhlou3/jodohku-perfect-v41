9dfbb0c61a3b3c8066ca167b2097ad03cd80566b
Your Name <you@example.com>
2026-04-27 03:31:08 +0800

    Build: Final Official Signing with jodohku.keystore (v7.8)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,12 +20,18 @@ android {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("../../jodohku.keystore")
+            storePassword = "jodohkumalaysia"
+            keyAlias = "jodohku"
+            keyPassword = "jodohkumalaysia"
+        }
+    }
+
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.jodohku.malaysia"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -33,9 +39,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
